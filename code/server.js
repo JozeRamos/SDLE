274ef0cba2ee0291c6dbe827b70cfb72733f100f
diogofonte <diogo.fonte2000@gmail.com>
2023-11-08 11:25:52 +0000

    changes in the structure

--- code/server.js
+++ code/server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-});
+});*/
 
 // define routes for handling shopping lists, reading/writing JSON files, and managing clients' data.
 // Define models, routes, and controllers for your application        --> this needs to be before the app.listen() call
@@ -20,13 +20,9 @@ app.listen(port, () => {
 //app.use('/api/shopping-lists', shoppingListRoutes);
 
 
-
-
 // CRUD Operations Implementation
 //In the routes, implement Create, Read, Update, and Delete (CRUD) operations for managing shopping lists
 //For example, create routes like /api/lists, /api/lists/:clientId, and so on
-
-
 
 
 // For handling JSON Files:
@@ -38,3 +34,13 @@ fs.writeFileSync('shopping-lists/main.json', JSON.stringify(shoppingList, null, 
 
 // Testing 
 // test the app locally by running 'node server.js' and accessing http://localhost:3000
+
+
+import { Node } from "./src/node.js";
+
+const PORT = process.env.PORT || 3000;
+
+const node = new Node();
+await node.init(PORT);
+
+//await node.loadAccounts();
