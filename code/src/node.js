cf85b4e3c05ffd53b985490fed3051c9c005c4c7
Domingos Santos <domingosjmsantos@gmail.com>
2023-11-12 18:31:02 +0000

    testing

--- code/src/node.js
+++ code/src/node.js
@@ -22,36 +22,35 @@ export class Node {
   executeShoppingList(port) {
     const jsonFilePath = './shopping-lists/onePerReplica.json';
     const shoppingList = new ShoppingList('onePerReplica', []);
-
+  
     // Load the shopping list from the JSON file
-    shoppingList.loadShoppingList()
-
-
+    shoppingList.loadShoppingList();
+  
     this.app.use(express.static('public'));
     this.app.use(express.json());
-
-    let shoppingListData = {};
-
-
+  
     this.app.get('/api/shopping-list', (req, res) => {
       // Send the shopping list data as JSON
       res.json(shoppingList.getData());
     });
   
-
     this.app.post('/update-list', (req, res) => {
       const { name, desiredQuantity, quantityBought } = req.body;
-
-      // Use the ShoppingList and Item classes to update quantities
-      const item = new Item(name, desiredQuantity, quantityBought);
-      shoppingList.updateItem(item);
-
-      // Store the updated shopping list
-      shoppingList.storeShoppingList();
-
-      res.json({ message: 'List updated successfully' });
+  
+      // Lógica para percorrer os itens da lista, atualizar o item relevante e salvar a lista
+      const updated = shoppingList.updateItem(new Item(name, desiredQuantity, quantityBought));
+  
+      if (updated) {
+        // Salvar a lista atualizada
+        fs.writeFileSync(jsonFilePath, JSON.stringify(shoppingList.getData(), null, 2), 'utf8');
+        res.json({ message: 'List updated successfully' });
+      } else {
+        res.status(400).json({ message: 'Item not found in the shopping list' });
+      }
     });
-    
-    console.log(`Server is running on port ${port}`);
+  
+    this.app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
   }
-}
+}  
