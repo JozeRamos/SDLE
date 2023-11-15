8b1da7fa991f299aeb2ac244b73741a961e43569
JozeRamos <up202005460@up.pt>
2023-11-15 10:55:45 +0000

    load Balancer

--- code/index.js
+++ code/index.js
@@ -1,30 +1,20 @@
-// define routes for handling shopping lists, reading/writing JSON files, and managing clients' data.
-// Define models, routes, and controllers for your application        --> this needs to be before the app.listen() call
-//const shoppingListRoutes = require('./routes/shoppingListRoutes');
-//app.use('/api/shopping-lists', shoppingListRoutes);
+// index.js
 
-
-// CRUD Operations Implementation
-//In the routes, implement Create, Read, Update, and Delete (CRUD) operations for managing shopping lists
-//For example, create routes like /api/lists, /api/lists/:clientId, and so on
-
-
-// For handling JSON Files:
-/*const fs = require('fs');
-const data = fs.readFileSync('shopping-lists/main.json', 'utf8'); // read from JSON File
-const shoppingList = JSON.parse(data);
-fs.writeFileSync('shopping-lists/main.json', JSON.stringify(shoppingList, null, 2)); // write to JSON File*/
-
-
-// Testing 
-// test the app locally by running 'node server.js' and accessing http://localhost:3000
-
-
+import express from 'express';
 import { Node } from "./src/node.js";
 
-const PORT = process.env.PORT || 3000;
+const app1 = express();
+const app2 = express();
 
-const node = new Node();
-await node.init(PORT);
+// Handler method 
+const handler = num => (req,res)=>{
+	const { method, url, headers, body } = req;
+	res.send('Response from server ' + num);
+}
 
-//await node.loadAccounts();
+// Create instances of the Node class and initialize with ports
+const node1 = new Node();
+node1.init(3000);
+
+const node2 = new Node();
+node2.init(3001);
